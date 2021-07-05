1f0eab48aea316af00c9eaee9e0fcff27c2ff878
RaghurajSawant <raghuraj.sawant@gmail.com>
2021-07-05 09:57:49 -0400

    Update median.py

--- Statistics/Measures_Of_Central_Tendency/Mode/median.py
+++ Statistics/Measures_Of_Central_Tendency/Mode/median.py
@@ -7,3 +7,11 @@ data.head(5)
 
 # Use the mean function on the 'Overall Marks' column of the dataset
 data["Overall Marks"].median()
+
+# Capturing the quartiles of the dataset
+Q1 = data["Overall Marks"].quantile(0.25)
+Q2 = data["Overall Marks"].quantile(0.50)
+Q3 = data["Overall Marks"].quantile(0.75)
+Q4 = data["Overall Marks"].quantile(1)
+
+print(f"Q1 : {Q1} \n Q2 : {Q2} \n Q3 : {Q3} \n Q4 : {Q4} \n")
